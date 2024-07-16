9f4a58952350984131ac223a7402aee678752f28
surajmishr <107803484+surajmishr@users.noreply.github.com>
2024-07-16 12:15:54 +0530

    Create Laravel-condition.php

---
+++ Laravel-condition.php
@@ -0,0 +1,8 @@
+if(count($data) > 1){   }
+if(isset($data) && count($data) > 1) {} 
+if($data == true){}
+if($data != ''){}
+if($data == null)
+if(empty($data) && $data == null){ }
+if(!empty($data) && $DAta != null){ }
+if((!is_null($from) && !is_null($to)) { }
